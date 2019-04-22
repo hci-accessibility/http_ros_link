63c00696fc3b5b9c5fba634d2f431b5481ee1852
Hani Awni <hani.awni@gmail.com>
2019-04-22 10:00:56 +0000

    create barebones nav HTTP route

--- rosnode.py
+++ rosnode.py
@@ -1,7 +1,20 @@
-from bottle import route, run, template
+import json
+from bottle import route, run, template, request, response
+from bottle import get, post
 
-@route('/hello/<name>')
-def index(name):
-    return template('<b>Hello {{name}}</b>!', name=name)
+@get('/')
+def index():
+    return '<b>Hello!</b> The accessibilitySLAM http-ros-link is functional!'
 
-run(host='localhost', port=8080)
+@post('/nav_xy_xy')
+def navigate():
+    j = request.json
+    prof = j['prof']
+    start = j['start']
+    goal = j['goal']
+    # CALL ROS SERVICE HERE
+    # returning json as string for right now, 
+    # return the route in the future instead
+    return json.dumps(request.json)
+
+run(host='localhost', port=8080) #should change to the port i chose earlier
