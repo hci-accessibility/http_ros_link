e9cab0f2e6e0448a2790ec99d16b89dd678263f2
Hani Awni <hani.awni@gmail.com>
2019-04-30 04:33:27 -0400

    finalize routes for all constraint combos with test data

--- http-link.py
+++ http-link.py
@@ -12,8 +12,9 @@ def navigate():
 
     j = request.json
     prof = j['profile']
-    canStair = not prof['stairs']
-    canHeavyDoor = not prof['heavy doors']
+    cantStair = True if prof['stairs'] == "true" else False
+    cantHeavyDoor = True if prof['heavy doors']== "true" else False
+    print("cantStair: %s cantHeavyDoor: %s" % (cantStair, cantHeavyDoor))
     start = j['start']
     goal = j['goal']
 
@@ -28,9 +29,9 @@ def navigate():
     outSE = (925 , 969)
     outSW = (37 , 997)
     inNW = (267 , 257)
-    inNE = (683 , 253)
+    inNE = (690 , 258)
     inSE = (687 , 649)
-    inSW = (275 , 641)
+    inSW = (261 , 584)
     loungeDoorNout = (695 , 375)
     loungeDoorNin = (817 , 375)
     loungeDoorSout = (703 , 575)
@@ -39,6 +40,9 @@ def navigate():
     LS2 = (430 , 700)
     LS3 = (541 , 700)
     LS4 = (623 , 679)
+    LS5 = (567 , 931)
+    LS6 = (546 , 785)
+    LS7 = (702 , 893)
     lectSW = (365 , 590)
     lectSE = (613 , 667)
     rampBot = (291 , 671)
@@ -47,6 +51,16 @@ def navigate():
     S = (475 , 1050)
     E = (953 , 455)
     W = (5 , 500)
+    stairsNEtop = (830 , 155)
+    stairsSEtop = (859 , 818)
+    stairsNWtop = (105 , 121)
+    outNES = (957 , 174)
+    outSEN = (959 , 782)
+    outSES = (759 , 1034)
+    outNEN = (798 , 4)
+    outNWN = (178 , 7)
+    outSWS = (206 , 1038)
+    foyer = (514 , 967)
 
 
 
@@ -54,6 +68,10 @@ def navigate():
     # TODO: returning hardcoded json for right now, 
 
 # ~~~~~~~~~~~~~~~~~~~ Hardcoded Routes
+ # 00 = neither constraint
+ # 01 = stairs constrained
+ # 10 = heavy doors constrained
+ # 11 = both constraints
     AB_00 =[ #can heavy doors & stairs
         outdoors,
         stairsNEtop,
@@ -91,7 +109,7 @@ def navigate():
     ]
     AB_11 = AB_01 #NO heavy doors, NO stairs, SAME as AB_01 because not cutting thru lecture hall.
 
-    AC_00 =[ #heavy doors, can stairs
+    AC_00 =[ #can heavy doors, can stairs
         outdoors,
         stairsNEtop,
         inNE,
@@ -102,7 +120,7 @@ def navigate():
         LS6,
         lectureHall
     ]
-    AC_01 =[ #heavy doors, NO stairs
+    AC_01 =[ #can heavy doors, NO stairs
         outdoors,
         outNES,
         outSEN,
@@ -111,7 +129,7 @@ def navigate():
         S,
         foyer,
         LS5,
-        LS6
+        LS6,
         lectureHall
     ]
     AC_10 =[ #NO heavy doors, can stairs
@@ -147,7 +165,7 @@ def navigate():
         lectureHall
     ]
 
-    BC_00 =[ #heavy doors, can stairs
+    BC_00 =[ #can heavy doors, can stairs
         lounge,
         loungeDoorNin,
         loungeDoorNout,
@@ -158,7 +176,7 @@ def navigate():
         LS6,
         lectureHall
     ]
-    BC_01 = BC_00 #heavy doors, NO stairs #SAME as BC_00 b/c BC_00 route involves no stairs
+    BC_01 = BC_00 #can heavy doors, NO stairs #SAME as BC_00 b/c BC_00 route involves no stairs
         
     BC_10 =[ #NO heavy doors, can stairs
         lounge,
@@ -182,24 +200,24 @@ def navigate():
     #lounge B
     #lecturehall C
 
-    if(canHeavyDoor and canStair):
+    if(cantHeavyDoor and cantStair):
         if(start['x']==outdoors[0] and start['y']==outdoors[1]):
             if(goal['x']==lounge[0] and goal['y']==lounge[1]):
-                plan = AB_00
+                plan = AB_11
             elif(goal['x']==lectureHall[0] and goal['y']==lectureHall[1]):
-                plan = AC_00
+                plan = AC_11
         elif(start['x']==lounge[0] and start['y']==lounge[1]):
             if(goal['x']==outdoors[0] and goal['y']==outdoors[1]):
-                plan = AB_00[::-1]
+                plan = AB_11[::-1]
             elif(goal['x']==lectureHall[0] and goal['y']==lectureHall[1]):
-                plan = BC_00
+                plan = BC_11
         elif(start['x']==lectureHall[0] and start['y']==lectureHall[1]):
             if(goal['x']==outdoors[0] and goal['y']==lectureHall[1]):
-                plan = AC_00[::-1]
+                plan = AC_11[::-1]
             elif(goal['x']==lounge[0] and goal['y']==lounge[1]):
-                plan = BC_00[::-1]
+                plan = BC_11[::-1]
 
-    if(canHeavyDoor and not canStair):
+    if(not cantHeavyDoor and cantStair):
         if(start['x']==outdoors[0] and start['y']==outdoors[1]):
             if(goal['x']==lounge[0] and goal['y']==lounge[1]):
                 plan = AB_01
@@ -216,7 +234,7 @@ def navigate():
             elif(goal['x']==lounge[0] and goal['y']==lounge[1]):
                 plan = BC_01[::-1]
 
-    if(not canHeavyDoor and canStair):
+    if(cantHeavyDoor and not cantStair):
         if(start['x']==outdoors[0] and start['y']==outdoors[1]):
             if(goal['x']==lounge[0] and goal['y']==lounge[1]):
                 plan = AB_10
@@ -233,22 +251,22 @@ def navigate():
             elif(goal['x']==lounge[0] and goal['y']==lounge[1]):
                 plan = BC_10[::-1]
 
-    if(not canHeavyDoor and not canStair):
+    if(not cantHeavyDoor and not cantStair):
         if(start['x']==outdoors[0] and start['y']==outdoors[1]):
             if(goal['x']==lounge[0] and goal['y']==lounge[1]):
-                plan = AB_11
+                plan = AB_00
             elif(goal['x']==lectureHall[0] and goal['y']==lectureHall[1]):
-                plan = AC_11
+                plan = AC_00
         elif(start['x']==lounge[0] and start['y']==lounge[1]):
             if(goal['x']==outdoors[0] and goal['y']==outdoors[1]):
-                plan = AB_11[::-1]
+                plan = AB_00[::-1]
             elif(goal['x']==lectureHall[0] and goal['y']==lectureHall[1]):
-                plan = BC_11
+                plan = BC_00
         elif(start['x']==lectureHall[0] and start['y']==lectureHall[1]):
             if(goal['x']==outdoors[0] and goal['y']==lectureHall[1]):
-                plan = AC_11[::-1]
+                plan = AC_00[::-1]
             elif(goal['x']==lounge[0] and goal['y']==lounge[1]):
-                plan = BC_11[::-1]
+                plan = BC_00[::-1]
 
         #~~~~~~~~~~~~~~~~~~~~~~~~~ CONVERT TO DICT FOR json.dumps
     route = []
